f386055f4c3b6b43d13ea3eff5eb1a1cb6d38f2d
imdanielkirk <imdanielkirk@gmail.com>
2019-09-11 20:18:47 -0800

    created migration

--- App.Db/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace App.Db
-{
-    public class Class1
-    {
-    }
-}
---
+++ App.Db/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using App.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Db
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Value> Values { get; set; }
+    }
+}
