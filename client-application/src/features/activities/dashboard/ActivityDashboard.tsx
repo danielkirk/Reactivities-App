f049443f9272221cd0956e381633752caf62c87e
imdanielkirk <imdanielkirk@gmail.com>
2019-09-24 22:06:16 -0800

    added details card for dashboard

--- client-application/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-application/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Grid, GridColumn } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
 import ActivityList from "./ActivityList";
+import ActivityDetails from "./details/ActivityDetails";
 
 interface IProps {
   activities: IActivity[];
@@ -13,6 +14,9 @@ const ActivityDashboard: React.FC<IProps> = ({ activities }) => {
       <GridColumn width={10}>
         <ActivityList activities={activities} />
       </GridColumn>
+      <Grid.Column width={6}>
+        <ActivityDetails />
+      </Grid.Column>
     </Grid>
   );
 };
